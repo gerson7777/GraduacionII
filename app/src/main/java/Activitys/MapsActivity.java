3f0684799bd55fb3ba011e5d6fba06b553139170
gerson7777 <gersonbreakgonzalez@gmail.com>
2018-07-26 11:31:17 -0600

    Agregnado el diseño del Login

--- app/src/main/java/Activitys/MapsActivity.java
+++ app/src/main/java/Activitys/MapsActivity.java
@@ -1,8 +1,9 @@
-package com.example.abyte.graduacionii;
+package Activitys;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.example.abyte.graduacionii.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
