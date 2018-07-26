b1c1db5e67ebed17cdca23f2480e9f59d7a5aaa9
gerson7777 <gersonbreakgonzalez@gmail.com>
2018-07-26 12:30:11 -0600

    Agregando el diseño a la vista Registro

--- app/src/main/java/Activitys/Registro.java
+++ app/src/main/java/Activitys/Registro.java
@@ -1,7 +1,9 @@
-package com.example.abyte.graduacionii;
+package Activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.abyte.graduacionii.R;
 
 public class Registro extends AppCompatActivity {
 
